b8c5c19606d73f38d6604a2e785250854bc2aa3d
Justin Rainey <rainey.justin80@gmail.com>
2024-05-24 20:18:04 -0400

    more tweaking with the url

--- visit_count.js
+++ visit_count.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const apiUrl = 'https://resumeapiapp.azurewebsites.net';
+    const apiUrl = 'https://resumeapiapp.azurewebsites.net/api/api_trig?code=mmMcNeqOz_dGPOmPyJxVqoRinVN5TcqaSx2oJbNufVsnAzFuoPFU-g%3D%3D';
 
     fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error('Network response was not ok: ' + response.statusText);
             }
             return response.json();
         })
         .then(data => {
-            document.getElementById('visitorCount').textContent = data.count;
+            document.getElementById('count').textContent = data.count;
         })
         .catch(error => {
             console.error('There has been a problem with your fetch operation:', error);
-            document.getElementById('visitorCount').textContent = 'Unavailable';
+            document.getElementById('count').textContent = 'Unavailable';
         });
 });
