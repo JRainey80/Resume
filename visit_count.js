40a5211bee6798d2c2be436c8f80197b33ff4d1d
Justin Rainey <rainey.justin80@gmail.com>
2024-07-02 10:49:27 -0400

    new approach

--- visit_count.js
+++ visit_count.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const apiUrl = 'https://resumeapiapp.azurewebsites.net/api/api_trig?code=mmMcNeqOz_dGPOmPyJxVqoRinVN5TcqaSx2oJbNufVsnAzFuoPFU-g%3D%3D';
+    const apiUrl = 'https://resumeapiapp.azurewebsites.net/api/api_trig?action=getApiKey';
 
     fetch(apiUrl)
         .then(response => {
@@ -9,10 +9,27 @@ document.addEventListener('DOMContentLoaded', function() {
             return response.json();
         })
         .then(data => {
-            document.getElementById('count').textContent = data.count;
+            const apiKey = data.apiKey;
+            const apiUrlWithKey = `https://resumeapiapp.azurewebsites.net/api/api_trig?code=${apiKey}`;
+
+            fetch(apiUrlWithKey)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok: ' + response.statusText);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    document.getElementById('count').textContent = data.count;
+                })
+                .catch(error => {
+                    console.error('There has been a problem with your fetch operation:', error);
+                    document.getElementById('count').textContent = 'Unavailable';
+                });
         })
         .catch(error => {
             console.error('There has been a problem with your fetch operation:', error);
             document.getElementById('count').textContent = 'Unavailable';
         });
 });
+
