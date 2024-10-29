aa008d20dd918cb578ae954241cddb10c20f4f19
Justin Rainey <rainey.justin80@gmail.com>
2024-10-29 14:00:46 -0400

    switching apiURL back

--- visit_count.js
+++ visit_count.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const apiUrl = 'https://api.rainey-cloud.com/api/api_trig';
+    const apiUrl = 'https://apiendpoint.azureedge.net/api/api_trig';
 
     fetch(apiUrl)
         .then(response => {
